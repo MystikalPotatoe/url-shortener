4ba92c5289d561e367afea915042798e97e5d7c0
MystikalPotatoe <fraserscobie@hotmail.com>
2017-03-21 22:34:40 +0000

    setting up basic UI and APIs

--- source/react/url-input.js
+++ source/react/url-input.js
@@ -1,3 +1,25 @@
 
 const React = require('react'),
       ReactDOMServer = require('react-dom/server');
+      
+class myLink extends React.Component {
+    render () {
+        return <input type="url" name="userlink" data-info="The link provided by the user" placeholder="Add your link here" required title="You need to provide a link." id="userlink"/>;
+    }
+}
+
+var Register = React.createClass({
+    render:function(){
+        return <form method="post" accept-charset="utf-8" action="/">
+                  <myLink />
+                  <button type="submit">Submit</button>    
+                </form>;
+    }
+
+});
+
+module.exports = {
+    register:function(msg){
+        return ReactDOMServer.renderToString(<Register msg={msg} />);
+    }
+};
--- source/server.js
+++ source/server.js
@@ -1,12 +1,31 @@
 'use strict';
 
 const express = require('express'),
-      mongodb = require('mongodb').MongoClient,
+      mongo = require('mongodb').MongoClient,
       url = process.env.MONGODB_URI,
       app = express(),
-      bodyParser = require('body-parser');
+      bodyParser = require('body-parser'),
+      appHome = require('./react/url-input'); 
       
 app.use(express.static('public'));
 app.use(bodyParser.urlencoded({extended:true}));
 
 app.set('view engine','ejs');
+
+app.route('/')
+    .get((req,res) => {
+        let body = appHome.home();
+        res.render('layout/basic',{body:body});
+    })
+    .post((req,res) => {
+        
+    });
+
+app.get('/link/:id', (req,res) => {
+    console.log(req.params.id);
+    /*
+    mongo.connect(url, (err,db) => {
+        if(err) throw err;
+        
+    }); */
+});
